3ddc2b666d55c9c6358b68fdcea44fc953adf4e2
Iason Sarantopoulos <iason@sarantop.com>
2018-07-11 16:30:23 +0300

    Add parameters on DDPG for save and restore models
    Add a parameter for the maximum models to save and a parameter for providing  a path to a model to be restored

--- baselines/ddpg/training.py
+++ baselines/ddpg/training.py
@@ -15,7 +15,7 @@ from mpi4py import MPI
 def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, param_noise, actor, critic,
     normalize_returns, normalize_observations, critic_l2_reg, actor_lr, critic_lr, action_noise,
     popart, gamma, clip_norm, nb_train_steps, nb_rollout_steps, nb_eval_steps, batch_size, memory,
-    tau=0.01, eval_env=None, param_noise_adaption_interval=50):
+    tau=0.01, eval_env=None, param_noise_adaption_interval=50, max_saved_models=1, existing_model=None):
     rank = MPI.COMM_WORLD.Get_rank()
 
     assert (np.abs(env.action_space.low) == env.action_space.high).all()  # we assume symmetric actions.
@@ -31,7 +31,7 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
 
     # Set up logging stuff only for a single worker.
     if rank == 0:
-        saver = tf.train.Saver(max_to_keep=100)
+        saver = tf.train.Saver(max_to_keep=max_saved_models)
     else:
         saver = None
 
